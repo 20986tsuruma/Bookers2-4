2ec17f1c8bac69e5afeaca9aa0d5c69a6751a985
20986tsuruma <toshinori1202@gmail.com>
2020-08-07 23:41:11 +0900

    Initial Commit Message

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root 'home#top'
+  get 'home/about' => 'home#about'
+
+
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users,only: [:show,:index,:edit,:update,:create]
+
+
+
+end
